6b80738a6bd109ede4a1d9da88ce96d09589da83
bilkeonur <onurbilke@hotmail.com>
2022-04-21 17:30:50 +0300

    Initial commit

--- UnderlinedTextField/ViewController.swift
+++ UnderlinedTextField/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtPhoneNumber: UITextField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        txtName.setUnderLine()
+        txtPhoneNumber.setUnderLine()
     }
+}
 
+extension UITextField
+{
+    func setUnderLine()
+    {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        border.borderColor = UIColor(hexString: "#ed9f9f").cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width + 50, height: self.frame.size.height)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+}
 
+extension UIColor
+{
+    convenience init(hexString: String)
+    {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        
+        switch hex.count
+        {
+            case 3: // RGB (12-bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            
+            default:
+                (a, r, g, b) = (255, 0, 0, 0)
+        }
+        
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
 }
 
